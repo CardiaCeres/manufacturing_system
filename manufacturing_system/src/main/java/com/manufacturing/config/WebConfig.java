8618c5add34fab02176871481d9862becd09f354
CardiaCeres <Chilia0324@gmail.com>
2025-05-31 09:40:30 +0800

    Update WebConfig.java

--- manufacturing_system/src/main/java/com/manufacturing/config/WebConfig.java
+++ manufacturing_system/src/main/java/com/manufacturing/config/WebConfig.java
@@ -7,10 +7,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @SuppressWarnings("null")
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // 設置允許跨域的路徑
-                .allowedOrigins("http://localhost:8082")  // 允許來自 localhost:8082 的請求
+                .allowedOrigins("https://manufacturing-system-springboot.onrender.com")  // 允許來自 localhost:8082 的請求
                 .allowedMethods("GET", "POST", "PUT", "DELETE")  // 允許的HTTP方法
                 .allowedHeaders("*")  // 設置允許的請求頭
                 .allowCredentials(true);  // 設置是否允許帶有憑證的請求
