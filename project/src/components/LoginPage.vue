661cfd632c6470347c622e170e00641a90d7273b
CardiaCeres <Chilia0324@gmail.com>
2025-06-01 00:16:44 +0800

    Update LoginPage.vue

--- project/src/components/LoginPage.vue
+++ project/src/components/LoginPage.vue
@@ -33,7 +33,7 @@ export default {
   methods: {
     async login() {
       try {
-        const response = await axios.post(`${API_BASE_URL}/api/login`, {
+        const response = await axios.post("https://manufacturing-system-springboot.onrender.com/api/login", {
           username: this.username,
           password: this.password
         });
