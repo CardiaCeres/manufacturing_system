8a27463f15d5804992e70c829a1c1bacb7e25236
CardiaCeres <Chilia0324@gmail.com>
2025-06-05 00:03:06 +0800

    Update OrderService.java

--- manufacturing_system/src/main/java/com/manufacturing/service/OrderService.java
+++ manufacturing_system/src/main/java/com/manufacturing/service/OrderService.java
@@ -19,9 +19,10 @@ public class OrderService {
         return orderRepository.findByUserIdOrderByOrderNumberAsc(userId);
     }
 
-    public Order createOrder(Order order) {
-        return orderRepository.save(order);
-    }
+   public List<Order> createOrder(Order order) {
+    orderRepository.save(order);
+    return orderRepository.findByUserIdOrderByOrderNumberAsc(order.getUserId());
+}
 
     public void deleteOrder(Long id) {
         orderRepository.deleteById(id);
