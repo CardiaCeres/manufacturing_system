3284f11ca7827c45aeaf92c95b153d0b3eeaab17
CardiaCeres <Chilia0324@gmail.com>
2025-07-26 17:06:48 +0800

    Update UserService.java

--- manufacturing_system/src/main/java/com/manufacturing/service/UserService.java
+++ manufacturing_system/src/main/java/com/manufacturing/service/UserService.java
@@ -1,33 +1,30 @@
 package com.manufacturing.service;
-
+ 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
+ 
 import com.manufacturing.model.User;
 import com.manufacturing.repository.UserRepository;
-
+ 
 @Service
 public class UserService {
-
+ 
     @Autowired
     private UserRepository userRepository;
-
-    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-
-    public User saveUser(User user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        return userRepository.save(user);
+ 
+    // 驗證使用者登入
+    public boolean validateUser(String username, String password) {
+        User existingUser = userRepository.findByUsername(username);
+        return existingUser != null && existingUser.getPassword().equals(password);
     }
-
-    public boolean validateUser(String username, String rawPassword) {
-        User user = userRepository.findByUsername(username);
-        if (user == null) return false;
-
-        return passwordEncoder.matches(rawPassword, user.getPassword());
-    }
-
+ 
+    // 根據使用者名稱取得使用者
     public User getUserByUsername(String username) {
         return userRepository.findByUsername(username);
+    }
+ 
+    // 註冊新使用者
+    public User saveUser(User user) {
+        return userRepository.save(user);
     }
 }
