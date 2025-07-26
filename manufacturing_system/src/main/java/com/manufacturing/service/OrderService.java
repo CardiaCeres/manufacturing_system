3a9ea6b5c535097a83bb18b938d189105c63f4df
CardiaCeres <Chilia0324@gmail.com>
2025-07-26 14:19:56 +0800

    Update OrderService.java

--- manufacturing_system/src/main/java/com/manufacturing/service/OrderService.java
+++ manufacturing_system/src/main/java/com/manufacturing/service/OrderService.java
@@ -19,16 +19,15 @@ public class OrderService {
         return orderRepository.findByUserIdOrderByOrderNumberAsc(userId);
     }
 
-   public List<Order> createOrder(Order order) {
-    orderRepository.save(order);
-    return orderRepository.findByUserIdOrderByOrderNumberAsc(order.getUserId());
-}
+    public Order createOrder(Order order) {
+        return orderRepository.save(order);
+    }
 
     public void deleteOrder(Long id) {
         orderRepository.deleteById(id);
     }
 
- public List<Order> updateOrder(Long id, Order order) {
+    public Order updateOrder(Long id, Order order) {
         Optional<Order> existingOrderOpt = orderRepository.findById(id);
         if (existingOrderOpt.isPresent()) {
             Order existingOrder = existingOrderOpt.get();
@@ -41,13 +40,12 @@ public class OrderService {
             existingOrder.setTotalAmount(order.getTotalAmount());
             existingOrder.setStatus(order.getStatus());
             existingOrder.setOrderDate(order.getOrderDate());
-
-            orderRepository.save(existingOrder);
  
-            return orderRepository.findByUserIdOrderByOrderNumberAsc(existingOrder.getUserId());
+            return orderRepository.save(existingOrder);
+
+            
         } else {
             throw new RuntimeException("訂單未找到，無法更新");
         }
     }
 }
- 
