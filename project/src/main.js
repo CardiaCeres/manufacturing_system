9b189a0c27da91e4d2eb8941885589371e6baa10
CardiaCeres <Chilia0324@gmail.com>
2025-07-27 12:41:22 +0800

    Update main.js

--- project/src/main.js
+++ project/src/main.js
@@ -1,10 +1,22 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import store from './store';
-import router from './router';
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import axios from 'axios'
 
-const app = createApp(App);
-app.use(store);
-app.use(router);
-app.mount('#app');
-app.config.globalProperties.$apiBaseUrl = "https://manufacturing-system-latest.onrender.com";
+axios.defaults.baseURL = 'https://manufacturing-system-latest.onrender.com'
+
+axios.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`
+    }
+    return config
+  },
+  error => Promise.reject(error)
+)
+
+const app = createApp(App)
+app.config.globalProperties.$axios = axios
+app.use(router)
+app.mount('#app')
