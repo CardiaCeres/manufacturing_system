f789e718cdf2c2154fcd0efb2e51855850518b3d
CardiaCeres <Chilia0324@gmail.com>
2025-07-27 12:55:14 +0800

    Update main.js

--- project/src/main.js
+++ project/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'https://manufacturing-system-latest.onrender.com'
+axios.defaults.baseURL = 'https://manufacturing-system-latest.onrender.com/api'
 
 axios.interceptors.request.use(
   config => {
