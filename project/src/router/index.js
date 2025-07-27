988d15f1b3ae3b49984c3bbaa68c3a4d0202acbe
CardiaCeres <Chilia0324@gmail.com>
2025-07-27 16:30:29 +0800

    Update index.js

--- project/src/router/index.js
+++ project/src/router/index.js
@@ -1,19 +1,49 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import LoginPage from '../components/LoginPage.vue';
-import OrderManagement from '../components/OrderManagement.vue';
-import RegisterPage from '../components/RegisterPage.vue';
-import HomeView from '../components/HomeView.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import Login from "@/views/Login.vue";
+import Register from "@/views/Register.vue";
+import OrderManagement from "@/views/OrderManagement.vue";
 
 const routes = [
-  { path: '/', name: 'Home', component: HomeView },
-  { path: '/login', name: 'Login', component: LoginPage },
-  { path: '/orders', name: 'Orders', component: OrderManagement },
-  { path: '/register', name: 'Register', component: RegisterPage }
+  {
+    path: "/login",
+    name: "Login",
+    component: Login
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register
+  },
+  {
+    path: "/orders",
+    name: "OrderManagement",
+    component: OrderManagement,
+    meta: { requiresAuth: true } // 需要登入才能訪問
+  },
+  {
+    path: "/",
+    redirect: "/orders"
+  },
+  {
+    path: "/:pathMatch(.*)*", // 404 頁面可自行建立
+    redirect: "/login"
+  }
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes
+});
+
+// 全域路由守衛：檢查 token，沒有就導到登入頁
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem("token");
+
+  if (to.meta.requiresAuth && !token) {
+    next({ name: "Login" });
+  } else {
+    next();
+  }
 });
 
 export default router;
