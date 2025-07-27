f6b271b749b51b139b9765fb16c68041014a9707
CardiaCeres <Chilia0324@gmail.com>
2025-07-27 08:59:20 +0800

    Update IndexController.java

--- manufacturing_system/src/main/java/com/manufacturing/controller/IndexController.java
+++ manufacturing_system/src/main/java/com/manufacturing/controller/IndexController.java
@@ -4,10 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
  
 @Controller
-public class IndexController {
- 
+public class ForwardController {
+
     @RequestMapping(value = {
-        "/", "/login","/register","/orders"
+        "/", "/login", "/register", "/orders", "/home",
+        "/{path:[^\\.]*}", "/**/{path:[^\\.]*}"
     })
     public String forward() {
         return "forward:/index.html";
