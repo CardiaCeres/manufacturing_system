0aec6bd77b93232b112e52e4d3e44b62a77bb796
CardiaCeres <Chilia0324@gmail.com>
2025-07-27 16:48:02 +0800

    Update index.js

--- project/src/router/index.js
+++ project/src/router/index.js
@@ -7,28 +7,13 @@ import HomeView from '../components/HomeView.vue';
 const routes = [
   { path: '/', name: 'Home', component: HomeView },
   { path: '/login', name: 'Login', component: LoginPage },
-  { path: '/orders', name: 'Orders', component: OrderManagement, meta: { requiresAuth: true } },
+  { path: '/orders', name: 'Orders', component: OrderManagement },
   { path: '/register', name: 'Register', component: RegisterPage }
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes
-});
-
-// 全局路由守衛
-router.beforeEach((to, from, next) => {
-  const token = localStorage.getItem('token');
-
-  // 需要登入才能進入的頁面
-  if (to.meta.requiresAuth && !token) {
-    next('/login'); // 沒 token 導向登入頁
-  } else if ((to.path === '/login' || to.path === '/register') && token) {
-    // 如果已登入，避免回到登入或註冊頁，導向首頁或訂單頁
-    next('/orders');
-  } else {
-    next(); // 其他路由正常放行
-  }
 });
 
 export default router;
