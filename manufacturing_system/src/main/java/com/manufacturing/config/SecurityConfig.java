28db9382af6d2b07776d8844d099435c6f04d9ca
CardiaCeres <Chilia0324@gmail.com>
2025-08-13 09:28:28 +0800

    Update SecurityConfig.java

--- manufacturing_system/src/main/java/com/manufacturing/config/SecurityConfig.java
+++ manufacturing_system/src/main/java/com/manufacturing/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.manufacturing.config;
 import com.manufacturing.security.JwtFilter;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
