a812f2f667d864d1fb434a1cd15a557abf0b2c4a
CardiaCeres <Chilia0324@gmail.com>
2025-08-14 23:35:13 +0800

    Update main.js

--- project/src/main.js
+++ project/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 
-axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL
+axios.defaults.baseURL = process.env.VUE_APP_BACKEND_URL
 
 axios.interceptors.request.use(
   config => {
