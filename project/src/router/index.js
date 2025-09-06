896592a4dac2bd47ed8c1822e8391b7613b483e6
CardiaCeres <Chilia0324@gmail.com>
2025-09-06 12:18:00 +0800

    Update index.js

--- project/src/router/index.js
+++ project/src/router/index.js
@@ -4,13 +4,15 @@ import OrderManagement from '../components/OrderManagement.vue';
 import RegisterPage from '../components/RegisterPage.vue';
 import HomeView from '../components/HomeView.vue';
 import ForgotPassword from '../components/ForgotPassword.vue'
+import ResetPassword from '../components/ResetPassword.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: HomeView },
   { path: '/login', name: 'Login', component: LoginPage },
   { path: '/orders', name: 'Orders', component: OrderManagement },
   { path: '/register', name: 'Register', component: RegisterPage },
-  { path: '/forgot-password', component: ForgotPassword }
+  { path: '/forgot-password', component: ForgotPassword },
+  { path: '/reset-password', component: ResetPassword }
 ];
 
 const router = createRouter({
