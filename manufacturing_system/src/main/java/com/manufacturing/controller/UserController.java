095a79e75f0b8d5fbdbca9ab52de584bbc8ff9a7
CardiaCeres <Chilia0324@gmail.com>
2025-10-06 01:41:27 +0800

    Update UserController.java

--- manufacturing_system/src/main/java/com/manufacturing/controller/UserController.java
+++ manufacturing_system/src/main/java/com/manufacturing/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.manufacturing.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
