37aea7a0c1e5216b601918bd1adadbafb14a9188
CardiaCeres <Chilia0324@gmail.com>
2025-12-15 18:07:00 +0800

    Update ManufacturingSystemApplication.java

--- manufacturing_system/src/main/java/com/manufacturing/ManufacturingSystemApplication.java
+++ manufacturing_system/src/main/java/com/manufacturing/ManufacturingSystemApplication.java
@@ -2,14 +2,8 @@ package com.manufacturing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
- 
-@SpringBootApplication(
-    exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class}
-)
 
-
+@SpringBootApplication
 public class ManufacturingSystemApplication {
 
 	public static void main(String[] args) {
